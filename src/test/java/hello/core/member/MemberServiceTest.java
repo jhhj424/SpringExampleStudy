5b4f42e3afb4becc8945627f4dfd641052941314
SHIN JIHO <jhhj424@naver.com>
2020-10-17 15:13:50 +0900

    관심사의 분리를 통해 이전 이슈들을 처리함, AppConfig 클래스에서 Service 객체들을 주입해줌

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,14 +1,22 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
-    public void join() {
+    void join() {
         //givne
         Member member = new Member(1L, "memberA", Grade.VIP);
 

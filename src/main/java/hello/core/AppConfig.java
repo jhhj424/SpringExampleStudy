14d560c2344b796e96f7646e43cb7caac0830b44
jiho <jhhj424@naver.com>
2020-10-20 17:40:31 +0900

    AppConfig discountPolicy를 FixDiscountPolicy로 변경

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,7 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
@@ -31,6 +32,7 @@ public class AppConfig {
 
     @Bean
     public DiscountPolicy discountPolicy() {
-        return new RateDiscountPolicy();
+//        return new RateDiscountPolicy();
+        return new FixDiscountPolicy();
     }
 }

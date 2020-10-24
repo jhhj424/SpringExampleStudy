17032b3385f85ef3279f8642cc4ee8443f5527a8
jiho <jhhj424@naver.com>
2020-10-24 15:13:02 +0900

    DI 컨테이너 객체 생성 테스트 - 싱글톤을 적용하지 않았을때

--- src/test/java/hello/core/singleton/singletonTest.java
+++ src/test/java/hello/core/singleton/singletonTest.java
@@ -2,13 +2,12 @@ package hello.core.singleton;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class singletonTest {
+class singletonTest {
 
     @Test
     @DisplayName("스프링 없는 순수한 DI 컨테이너")

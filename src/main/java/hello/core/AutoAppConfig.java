243b23a258ef4f10b77b307414f49e02ff700d69
jiho <jhhj424@naver.com>
2020-11-28 18:28:27 +0900

    feat: 컴포넌트 스캔 - 탐색 위치와 기본 스캔 대상
    스프링 부트는 루트부터 스캔 - @SpringbootApplication에 @ComponentScan이 포함되어있음

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration // 설정정보
-@ComponentScan(
+@ComponentScan( // 기본 설정은 : @ComponentScan이 붙은 설정정보 클래스의 패키지가 스캔 시작 패키지가 된다.
+        // basePackages = "hello.core", // 탐색할 패키지의 시작위치 지정, 해당 패키지부터 하위 패키지 모두 탐색 (여러개도가능)
+        // basePackageClasses = AutoAppConfig.class, // 해당 클래스의 패키지부터 스캔
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // 대상 제외
 ) // @Component 애노테이션이 붙은 클래스를 스캔해서 스프링 빈으로 등록한다
 public class AutoAppConfig {
